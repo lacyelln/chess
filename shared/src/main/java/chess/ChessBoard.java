c0270ed26d210f80650886e29d0bbe94fbab4e38
Lacy Miller <lacyellenmiller@gmail.com>
2024-09-12 15:40:58 -0600

    init(phase0)

---
+++ shared/src/main/java/chess/ChessBoard.java
@@ -0,0 +1,48 @@
+package chess;
+
+/**
+ * A chessboard that can hold and rearrange chess pieces.
+ * <p>
+ * Note: You can add to this class, but you may not alter
+ * signature of the existing methods.
+ */
+public class ChessBoard {
+
+    private ChessPiece[][] squares = new ChessPiece[8][8];
+
+    public ChessBoard() {
+        
+    }
+
+    /**
+     * Adds a chess piece to the chessboard
+     *
+     * @param position where to add the piece to
+     * @param piece    the piece to add
+     */
+    public void addPiece(ChessPosition position, ChessPiece piece) {
+        squares[position.getRow()][position.getColumn()] = piece;
+    }
+
+    /**
+     * Gets a chess piece on the chessboard
+     *
+     * @param position The position to get the piece from
+     * @return Either the piece at the position, or null if no piece is at that
+     * position
+     */
+    public ChessPiece getPiece(ChessPosition position) {
+        if (squares[position.getRow()][position.getColumn()] != null) {
+            return squares[position.getRow()][position.getColumn()]; //im not sure if that is returning the chess piece.
+        }
+        return null;
+    }
+
+    /**
+     * Sets the board to the default starting board
+     * (How the game of chess normally starts)
+     */
+    public void resetBoard() {
+        squares = new ChessPiece[8][8];
+    }
+}
