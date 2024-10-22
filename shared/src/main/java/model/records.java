be82cd1faa790660cea17993ca47b0bfcfc86e9b
Lacy Miller <lacyellenmiller@gmail.com>
2024-10-22 15:48:26 -0600

    Updated records for Game, User and Auth

--- shared/src/main/java/model/records.java
+++
@@ -1,6 +0,0 @@
-package model;
-import chess.ChessGame;
-
-record UserData(String username, String password, String email);
-record GameData(int GameID, String whiteUsername, String blackUsername, String gameName, ChessGame game);
-record AuthData(String authToken, String username);
