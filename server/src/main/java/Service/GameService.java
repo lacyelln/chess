0b506a4637fe075706e3fec261cddb3cf54910d7
Lacy Miller <lacyellenmiller@gmail.com>
2024-10-22 19:34:55 -0600

    Implemented 4 endpoints in GameService

---
+++ server/src/main/java/Service/GameService.java
@@ -0,0 +1,61 @@
+package Service;
+
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.ArrayList;
+
+public class GameService {
+
+    private final AuthDAO aData;
+    private final GameDAO gData;
+    private final UserDAO uData;
+
+    public GameService(AuthDAO aData, GameDAO gData, UserDAO uData){
+        this.aData = aData;
+        this.gData = gData;
+        this.uData = uData;
+    }
+
+    public GameData createGame(AuthData auth, GameData game) throws DataAccessException{
+        UserData user = new UserData(auth.username(), null, null);
+        if(aData.getAuth(user) == null){
+            throw new DataAccessException("Error: unauthorized");
+        }
+        if(gData.createGame(game) == null){
+            throw new DataAccessException("Error: bad request");
+        }
+        return gData.createGame(game);
+    }
+
+    public ArrayList<GameData> listGames(AuthData auth) throws DataAccessException{
+        UserData user = new UserData(auth.username(), null, null);
+        if(aData.getAuth(user) == null){
+            throw new DataAccessException("Error: unauthorized");
+        }
+        return gData.listGames();
+    }
+
+    public void joinGame(AuthData auth, GameData game) throws DataAccessException{
+        UserData user = new UserData(auth.username(), null, null);
+        if(aData.getAuth(user) == null){
+            throw new DataAccessException("Error: unauthorized");
+        }
+        if(gData.getGame(game) == 0){
+            throw new DataAccessException("Error: bad request");
+        }
+        gData.updateGame(game);
+    }
+
+    public void delete() throws DataAccessException{
+        gData.deleteAllGames();
+        aData.deleteAllAuth();
+        uData.deleteAllUsers();
+    }
+}
+
