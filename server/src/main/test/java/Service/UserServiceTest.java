4389fdcc15aa0932a90feeb0a0f548c772842ff1
Lacy Miller <lacyellenmiller@gmail.com>
2024-10-23 10:41:38 -0600

    Added register test

--- server/src/main/test/java/Service/UserServiceTest.java
+++ server/src/main/test/java/Service/UserServiceTest.java
@@ -1,5 +1,10 @@
 package Service;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
+import dataaccess.UserDAO;
+import model.UserData;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,8 +12,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserServiceTest {
 
     @Test
-    void register() {
-        assertTrue(true);
+    void register() throws DataAccessException {
+        var user = new UserData("a", "a", "a");
+        var mUser = new MemoryUserDAO();
+        var mAuth = new MemoryAuthDAO();
+        UserService service = new UserService(mUser, mAuth);
+        var authData = service.register(user);
+        assertEquals(user.username(), authData.username());
+
+
     }
 
     @Test
