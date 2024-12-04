868456828583f07c7e0203d2d40f5ea8b98cdb16
Lacy <lacyellenmiller@gmail.com>
2024-12-04 11:23:18 -0700

    Updated WebSocketFacade

--- client/src/main/java/ui/WebSocket/WebSocketFacade.java
+++ client/src/main/java/ui/WebSocket/WebSocketFacade.java
@@ -6,63 +6,27 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import spark.Session;
 import ui.ResponseException;
 import websocket.commands.UserGameCommand;
 import websocket.messages.Action;
 import websocket.messages.ServerMessage;
 
-\
+
 //need to extend Endpoint for websocket to work properly
-public class WebSocketFacade extends Endpoint {
+public class WebSocketFacade {
 
     Session session;
     NotificationHandler notificationHandler;
 
 
-    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws ResponseException {
-        try {
-            url = url.replace("http", "ws");
-            URI socketURI = new URI(url + "/ws");
-            this.notificationHandler = notificationHandler;
-
-            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-            this.session = container.connectToServer(this, socketURI);
-
-            //set message handler
-            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
-                @Override
-                public void onMessage(String message) {
-                    Notification notification = new Gson().fromJson(message, Notification.class);
-                    notificationHandler.notify(notification);
-                }
-            });
-        } catch (DeploymentException | IOException | URISyntaxException ex) {
-            throw new ResponseException(500, ex.getMessage());
+    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws ResponseException, URISyntaxException {
+        url = url.replace("http", "ws");
+        URI socketURI = new URI(url + "/ws");
+        this.notificationHandler = notificationHandler;
         }
+
+        //Endpoint requires this method, but you don't have to do anything
+
     }
 
-    //Endpoint requires this method, but you don't have to do anything
-    @Override
-    public void onOpen(Session session, EndpointConfig endpointConfig) {
-    }
-
-    public void enter(String visitorName) throws ResponseException {
-        try {
-            var action = new Action(Action.Type.ENTER, visitorName);
-            this.session.getBasicRemote().sendText(new Gson().toJson(action));
-        } catch (IOException ex) {
-            throw new ResponseException(ex.getMessage());
-        }
-    }
-
-    public void leave(String visitorName) throws ResponseException {
-        try {
-            var action = new Action(Action.Type.EXIT, visitorName);
-            this.session.getBasicRemote().sendText(new Gson().toJson(action));
-            this.session.close();
-        } catch (IOException ex) {
-            throw new ResponseException(500, ex.getMessage());
-        }
-    }
-
-}
