d667598f73775f71e1b9391d896639b3f2e35b80
Lacy <lacyellenmiller@gmail.com>
2024-12-04 11:34:13 -0700

    Trying to get errors out of WebSocketFacade

--- client/src/main/java/ui/WebSocket/WebSocketFacade.java
+++ client/src/main/java/ui/WebSocket/WebSocketFacade.java
@@ -3,30 +3,20 @@ package ui.WebSocket;
 
 import com.google.gson.Gson;
 import java.io.IOException;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import spark.Session;
+import webSocketMessages.Action;
+import webSocketMessages.Notification;
+
+import javax.websocket.*;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import spark.Session;
-import ui.ResponseException;
-import websocket.commands.UserGameCommand;
-import websocket.messages.Action;
-import websocket.messages.ServerMessage;
-
-
 //need to extend Endpoint for websocket to work properly
-public class WebSocketFacade {
-
-    Session session;
-    NotificationHandler notificationHandler;
+public class WebSocketFacade extends Endpoint {
 
 
-    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws ResponseException, URISyntaxException {
-        url = url.replace("http", "ws");
-        URI socketURI = new URI(url + "/ws");
-        this.notificationHandler = notificationHandler;
-        }
-
-        //Endpoint requires this method, but you don't have to do anything
-
-    }
-
+}
